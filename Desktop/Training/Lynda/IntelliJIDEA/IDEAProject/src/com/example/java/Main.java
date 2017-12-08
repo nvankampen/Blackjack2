8679eaab5ea716345ee843f730e7a4a144ef4a09
Nathan Van Kampen <nvankampen@xpanxion.com>
2017-12-08 13:43:58 -0600

    simple calculator

--- Desktop/Training/Lynda/IntelliJIDEA/IDEAProject/src/com/example/java/Main.java
+++ Desktop/Training/Lynda/IntelliJIDEA/IDEAProject/src/com/example/java/Main.java
@@ -1,10 +1,26 @@
 package com.example.java;
 
+import javax.xml.bind.SchemaOutputResolver;
+import java.util.Scanner;
+
 public class Main {
 
     public static void main(String[] args) {
-	    System.out.println("Hello from IDEA!");
-	    String aString = "Nathan";
-        System.out.println("Your name is " +aString);
-    }
+    	String firstValue;
+	    String secondValue;
+
+		Scanner in = new Scanner(System.in);
+		System.out.println("Enter in two numbers to add together");
+		System.out.println("Enter first number");
+		firstValue = in.nextLine();
+		System.out.println("Enter a second number");
+		secondValue = in.nextLine();
+
+		int firstInt = Integer.parseInt(firstValue);
+		int secondInt = Integer.parseInt(secondValue);
+
+		int intTotal = firstInt + secondInt;
+		System.out.println("The total is " + intTotal);
+
+	}
 }
